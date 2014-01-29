8b217971c80c76b414942689c0903f8806c526b0
Jasonmrc <jasonmrc@gmail.com>
2014-01-28 19:17:46 -0500

    Updated Help.

--- client/help.lua
+++ client/help.lua
@@ -1,20 +1,22 @@
 function ModulesLoad()
     Events:Fire( "HelpAddItem",
         {
-            name = "Vertical Take Off and Landing",
+            name = "Vertical Take-Off and Landing",
             text = 
                 "Ever wanted to do a vertical take-off or reverse in a plane? Now you can!\n"..
 				"It is easy to take-off, but vertical landing can be tricky.\n\n"..
 				"Use Z to do a Vertical Take-Off or Hover for Vertical Landing.\n"..
 				"Use X to Reverse Thrust.\n"..
-				"Use CTRL when Landing to activate the Landing gear.\n"
+				"Use W and S to keep the nose and tail level when vertically landing.\n"..
+				"Landing Gear automatically activate when using the Vertical Take-Off/Landing key and you are within 35m to a surface.\n"..
+				"If enabled by the Admin, this Mod also allows EasyLand, which automatically slows planes when they are near the ground.\n"
         } )
 end
 
 function ModuleUnload()
     Events:Fire( "HelpRemoveItem",
         {
-            name = "Vertical Take Off and Landing"
+            name = "Vertical Take-Off and Landing"
         } )
 end
 
